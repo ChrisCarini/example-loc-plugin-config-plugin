e6f84cfdd634c0cc5a458fa260b9b611059d38f5
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-05 15:47:01 -0700

    Initial meaningful commit

---
+++ src/main/java/com/chriscarini/jetbrains/examplelocconfigurationplugin/LoCThresholdConfigurer.java
@@ -0,0 +1,46 @@
+package com.chriscarini.jetbrains.examplelocconfigurationplugin;
+
+
+import com.chriscarini.jetbrains.locchangecountdetector.ChangeThresholdIconInfo;
+import com.chriscarini.jetbrains.locchangecountdetector.ChangeThresholdService;
+import com.chriscarini.jetbrains.locchangecountdetector.ChangeThresholdTimeInfo;
+import com.chriscarini.jetbrains.locchangecountdetector.LoCService;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Configures LoC Thresholds for the default project.
+ */
+public class LoCThresholdConfigurer implements StartupActivity, StartupActivity.DumbAware {
+    private static final @NonNls Logger LOG = Logger.getInstance(LoCThresholdConfigurer.class);
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        projectOpened(project);
+    }
+
+    public void projectOpened(@NotNull Project project) {
+        if (!ApplicationManager.getApplication().isUnitTestMode()) {
+            final ChangeThresholdService changeThresholdService = ChangeThresholdService.getInstance(project);
+            changeThresholdService.setChangeThresholdTimeInfos(
+                new ArrayList<>(List.of(
+                    new ChangeThresholdTimeInfo("S", 10, 1, 2),
+                    new ChangeThresholdTimeInfo("M", 20, 3, 4),
+                    new ChangeThresholdTimeInfo("L", Integer.MAX_VALUE, 5, 6)
+                ))
+            );
+            changeThresholdService.setChangeThresholdIconInfo(
+                new ChangeThresholdIconInfo(100, 75, 50)
+            );
+            LoCService.getInstance(project).computeLoCInfo();
+        }
+    }
+}
